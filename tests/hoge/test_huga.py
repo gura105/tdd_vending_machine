61063ee78444fb696ed5a5a2d406a66bf0ff094d
dnoo <ndoagiuscuhkie@gmail.com>
2020-08-06 09:20:40 +0900

    first_test_daisuke

--- tests/hoge/test_huga.py
+++ tests/hoge/test_huga.py
@@ -4,3 +4,8 @@ from vending_machine.hoge.huga import Huga
 class TestHuga:
     def test_huga(self):
         assert Huga().piyo() == 'piyo'
+
+
+class DasiukeHuga:
+    def dai_huga(self):
+        pass
